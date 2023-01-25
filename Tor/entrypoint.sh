07e22cea96c3ecf8a38c39cb9079baad18431859
Kamuri Amorim <myk.gata14@gmail.com>
2023-01-25 08:44:48 -0300

    feature: create container for tor

---
+++ Tor/entrypoint.sh
@@ -0,0 +1,9 @@
+service tor start
+service tor status
+while [[ "$(curl --socks5 '127.0.0.1:9050' -Ls -o /dev/null -w ''%{http_code}'' https://check.torproject.org)" != "200" ]]; do
+    echo "Waiting Tor..."
+    service tor start
+    service tor status
+    sleep 5
+done
+python main.py
