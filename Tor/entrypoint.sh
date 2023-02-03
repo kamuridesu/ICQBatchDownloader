e5161c63f0a2f6ab312125d03fa36663d68076f8
Kamuri Amorim <myk.gata14@gmail.com>
2023-02-03 15:43:06 -0300

    fix: fixing polling request for new api

--- Tor/entrypoint.sh
+++ Tor/entrypoint.sh
@@ -1,6 +1,6 @@
 service tor start
 service tor status
-while [[ "$(curl --socks5 '127.0.0.1:9050' -Ls -o /dev/null -w ''%{http_code}'' https://check.torproject.org)" != "200" ]]; do
+while [[ "$(curl --socks5 '127.0.0.1:9050' --connect-timeout 15 --max-time 30 -Ls -o /dev/null -w ''%{http_code}'' https://check.torproject.org)" != "200" ]]; do
     echo "Waiting Tor..."
     service tor start
     service tor status
