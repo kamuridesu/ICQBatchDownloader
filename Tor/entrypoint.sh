a3f46b8ad9a8a49b8f58def0d1c89bc680abeb9e
Kamuri Amorim <myk.gata14@gmail.com>
2023-02-03 15:55:30 -0300

    fix(tor): docker entrypoint

--- Tor/entrypoint.sh
+++ Tor/entrypoint.sh
@@ -2,7 +2,6 @@ service tor start
 service tor status
 while [[ "$(curl --socks5 '127.0.0.1:9050' --connect-timeout 15 --max-time 30 -Ls -o /dev/null -w ''%{http_code}'' https://check.torproject.org)" != "200" ]]; do
     echo "Waiting Tor..."
-    service tor start
     service tor status
     sleep 5
 done
--- src/infos.py
+++ src/infos.py
@@ -3,7 +3,7 @@ import json
 
 
 async def saveInfos(file_info: dict) -> None:
-    path = os.path.join("./data/", "file_infos.json")
+    path = os.path.join("./data/", "chat_infos.json")
     old_file_info = await loadInfos()
     with open(path, "w") as f:
         old_file_info.append(file_info)
@@ -11,7 +11,7 @@ async def saveInfos(file_info: dict) -> None:
 
 
 async def loadInfos() -> list:
-    path = os.path.join("./data/", "file_infos.json")
+    path = os.path.join("./data/", "chat_infos.json")
     if os.path.exists(path):
         with open(path, "r") as f:
             return json.loads(f.read())
